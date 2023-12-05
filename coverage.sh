42c36d98d8fe9ae9573af31d2b4a83bf196437d4
Fernando da Silva Sousa <wild.nando@gmail.com>
2023-12-04 22:05:49 -0300

    fix base directory issue

--- coverage.sh
+++ coverage.sh
@@ -6,12 +6,11 @@ meson test -C build
 
 cp /usr/share/vala-0.56/vapi/glib-2.0.vapi build/unit.p
 
-cd build/unit.p
-find ./ -type f -name "*.c" -exec cp {} . \;
+find ./build/unit.p -type f -name "*.c" -exec cp {} build/unit.p \;
 
 gcovr --version
 
-gcovr -r $ROOT --txt --jacoco jacoco.xml -o $ROOT/build/meson-logs/ --gcov-ignore-errors=all
+gcovr -r $ROOT --txt --jacoco -o $ROOT/build/meson-logs/ --gcov-ignore-errors=all
 
 cat $ROOT/build/meson-logs/coverage.txt
 rm $ROOT/build/meson-logs/coverage.txt
