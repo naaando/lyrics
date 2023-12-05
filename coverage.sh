ce5fa1e61bec74fca3be47a3d20b8457bafdd1bb
Fernando da Silva Sousa <wild.nando@gmail.com>
2023-12-04 21:36:09 -0300

    display gcovr version to debug

--- coverage.sh
+++ coverage.sh
@@ -9,6 +9,8 @@ cp /usr/share/vala-0.56/vapi/glib-2.0.vapi build/unit.p
 cd build/unit.p
 find ./ -type f -name "*.c" -exec cp {} . \;
 
+gcovr --version
+
 gcovr -r $ROOT --txt --sonarqube -o $ROOT/build/meson-logs/ --gcov-ignore-errors=all
 
 cat $ROOT/build/meson-logs/coverage.txt
