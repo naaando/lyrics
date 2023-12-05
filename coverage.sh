b083fb2e7f59d8445862fb782955ea702cd3a452
Fernando da Silva Sousa <wild.nando@gmail.com>
2023-12-04 23:03:30 -0300

    Use cobertura instead of jacoco

--- coverage.sh
+++ coverage.sh
@@ -8,7 +8,7 @@ find ./build/unit.p -type f -name "*.c" -exec cp {} build/unit.p \;
 
 gcovr --version
 
-gcovr -r $PWD --txt --jacoco -o $PWD/build/meson-logs/ --gcov-ignore-errors=all
+gcovr -r $PWD --txt --cobertura-pretty -o $PWD/build/meson-logs/ --gcov-ignore-errors=all
 
 cat $PWD/build/meson-logs/coverage.txt
 rm $PWD/build/meson-logs/coverage.txt
